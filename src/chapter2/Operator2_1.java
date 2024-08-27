c66e68fb4f04ddfd2cc801392aa364475d5a94d9
“Mikihisa <micchie09rtj@gmail.com>
2024-08-27 17:22:24 +0900

    パッケージの上のコメントを削除と、JavaDocの追記

--- src/chapter2/Operator2_1.java
+++ src/chapter2/Operator2_1.java
@@ -1,11 +1,15 @@
-//	コンソールに”Hello World!”を表示するプログラムを作成してください
-
 package chapter2;
+/**
+ * コンソールに”Hello World!”を表示するプログラムを作成。
+ */
 
 public class Operator2_1 {
-
+	/**
+	 * mainメソッド
+	 * Hello World!と出力させる。
+	 * @param args 使用しない。
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
-
 }
--- src/chapter2/Operator2_4.java
+++ src/chapter2/Operator2_4.java
@@ -1,15 +1,18 @@
-//以下処理を実装してください。
-//
-//int 型の変数 x に数値 7 を代入する。
-//変数 x の値を 3 倍にする。
-//変数 x の値を表示する。
-//変数 x の値を半分(1/2)にする。
-//変数 x の値を表示する。
-
 package chapter2;
-
+/**
+ * 以下処理を実装してください。
+ * int 型の変数 x に数値 7 を代入する。
+ * 変数 x の値を 3 倍にする。
+ * 変数 x の値を表示する。 
+ * 変数 x の値を半分(1/2)にする。
+ * 変数 x の値を表示する。 
+ */
 public class Operator2_4 {
-
+	/**
+	 * mainメソッド
+	 * 変数 x に7を代入し、3倍しその結果を表示、xを半分にし、その結果を表示。
+	 * @param args 使用しない。
+	 */
 	public static void main(String[] args) {
 		int x = 7;
 		
@@ -17,8 +20,6 @@ public class Operator2_4 {
 		System.out.println(x);
 		
 		x = x / 2;
-		System.out.println(x);		
-
+		System.out.println(x);
 	}
-
 }
